fdae00a3641e2f6af3cefb022da52a67d75ebe77
Hugo Oyola <hugo212h@gmail.com>
2024-04-09 17:54:17 -0500

    ✨ Agregar funcionalidad de selección de categoría y mostrar recetas en app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,75 @@
-function iniciarApp(){
+function iniciarApp() {
+  const selectCategorias = document.querySelector("#categorias");
+  selectCategorias.addEventListener("change", seleccionarCategoria);
 
-  const selectCategorias = document.querySelector('#categorias');
+  const resultado = document.querySelector("#resultado");
 
   obtenerCategorias();
 
-  function obtenerCategorias(){
-    const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+  function obtenerCategorias() {
+    const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
     fetch(url)
-      .then(respuesta => respuesta.json())
-      .then(resultado => mostrarCategorias(resultado.categories))
+      .then((respuesta) => respuesta.json())
+      .then((resultado) => mostrarCategorias(resultado.categories));
   }
 
-  function mostrarCategorias(categorias = []){
-    categorias.forEach(categoria => {
+  function mostrarCategorias(categorias = []) {
+    categorias.forEach((categoria) => {
+      const { strCategory } = categoria;
+      const option = document.createElement("OPTION");
+      option.value = strCategory;
+      option.textContent = strCategory;
+      selectCategorias.appendChild(option);
+    });
+  }
 
+  function seleccionarCategoria(e) {
+    const categoria = e.target.value;
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+    fetch(url)
+      .then((respuesta) => respuesta.json())
+      .then((resultado) => mostrarRecetas(resultado.meals));
+  }
+
+  function mostrarRecetas(recetas = []) {
+    // Iterar las recetas
+    recetas.forEach((receta) => {
+      const { strMeal, strMealThumb, idMeal } = receta;
+
+      const recetaContenedor = document.createElement("DIV");
+      recetaContenedor.classList.add("col-md-4");
+
+      const recetaCard = document.createElement("DIV");
+      recetaCard.classList.add("card", "mb-4", "shadow-sm");
+
+      const recetaImagen = document.createElement("IMG");
+      recetaImagen.classList.add("card-img-top");
+      recetaImagen.lastChild = `Imagen de la receta ${strMeal}`;
+      recetaImagen.src = strMealThumb;
+
+      const recetaCardBody = document.createElement("DIV");
+      recetaCardBody.classList.add("card-body");
+
+      const recetaHeading = document.createElement("H3");
+      recetaHeading.classList.add("card-title", "mb-3");
+      recetaHeading.textContent = strMeal;
+
+      const recetaBoton = document.createElement("BUTTON");
+      recetaBoton.classList.add("btn", "btn-danger", "w-100");
+      recetaBoton.textContent = "Ver receta";
+
+      // Inyectar en el codigo HTML
+      recetaCardBody.appendChild(recetaHeading);
+      recetaCardBody.appendChild(recetaBoton);
+
+      recetaCard.appendChild(recetaImagen);
+      recetaCard.appendChild(recetaCardBody);
+
+      recetaContenedor.appendChild(recetaCard);
+
+      resultado.appendChild(recetaContenedor);
     })
   }
 }
 
-document.addEventListener('DOMContentLoaded', iniciarApp)
+document.addEventListener("DOMContentLoaded", iniciarApp);
